def88bddef6c0b33c697866a442cdeb460cf9492
seril <45784494+lcandy2@users.noreply.github.com>
2022-05-09 23:25:46 +0800

    reduce permission

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
-chrome.runtime.onInstalled.addListener((reason) => {
-	if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+
+chrome.runtime.onInstalled.addListener(details => {
+	if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
 		chrome.tabs.create({ url: 'help.html' });
 	}
 });
