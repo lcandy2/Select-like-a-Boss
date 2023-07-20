bb14a84d540a47a51f48b5e136a414a2628f359c
甜檸seril <45784494+lcandy2@users.noreply.github.com>
2023-07-20 23:39:38 +0800

    Compatibility update

--- src/Select-like-a-Boss.js
+++ src/Select-like-a-Boss.js
@@ -1,92 +1,158 @@
-// Create and insert style element
-const styleElm = document.createElement('style');
-document.head.appendChild(styleElm);
-styleElm.sheet.insertRule('.ext-Select-like-a-Boss {user-select: text !important; outline-width: 0 !important;}', 0);
+const _bind = (evt, bind = true) => {
+  const events = Array.isArray(evt) ? evt : [evt];
+  const method = bind ? 'addEventListener' : 'removeEventListener';
+  events.forEach(e => document[method](e, handlers[e], true));
+};
+const _unbind = (evt) => _bind(evt, false);
 
-const getRangeFromPoint = (x, y) =>
-  document.caretPositionFromPoint
-    ? (() => {
-      const range = document.createRange();
-      const p = document.caretPositionFromPoint(x, y);
-      range.setStart(p.offsetNode, p.offset);
-      return range;
-    })()
-    : document.caretRangeFromPoint(x, y);
+function getCurrentAnchor(n) {
+  while (n && n !== document.body) {
+    if (n instanceof HTMLAnchorElement) return n;
+    n = n.parentNode;
+  }
+  return null;
+}
 
 const stopEvent = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  return false;
+  return e.preventDefault(), e.stopPropagation(), false;
 };
+
+// browser compatibility
+const getRangeFromPoint = (x, y) => {
+  if (document.caretPositionFromPoint) {
+    let range = document.createRange();
+    let p = document.caretPositionFromPoint(x, y);
+    range.setStart(p.offsetNode, p.offset);
+    return range;
+  } else return document.caretRangeFromPoint(x, y);
+}
+
+// user style
+const _letUserSelect = (function () {
+  let n,
+    styleElm = document.createElement('style');
+  let _className = 'ext-Select-like-a-Boss',
+    _property = '-webkit-user-select:text!important;outline-width:0!important;';
+  document.head.appendChild(styleElm);
+  styleElm.sheet.insertRule(`.${_className}{${_property}}`, 0);
+  return (node) => {
+    if (node) {
+      (n = node).classList.add(_className);
+    } else if (n) {
+      n.classList.remove(_className);
+      n = null;
+    }
+  };
+})();
 
 let selection = document.getSelection();
-let cursor = {}, userSelecting;
-let justSelected = false;
+let cursor = {},
+  movable,
+  needDetermineUserSelection,
+  needCreateStartSelection,
+  needStopClick,
+  userSelecting,
+  regexTDTH = /T[HD]/;
 
-const handlers = {
-  mousemove: (e) => {
-    let x = e.clientX, y = e.clientY;
-    let vx = Math.abs(cursor.x - x), vy = Math.abs(cursor.y - y);
-    userSelecting = vy === 0 || vx / vy > 0.8;
-    if (userSelecting && (vx > 3 || vy > 3)) {
-      let range = getRangeFromPoint(x, y);
-      if (range && selection.rangeCount > 0) {  // Check if a selection range exists
-        selection.extend(range.startContainer, range.startOffset);
-      }
-    }
-  },
-  mouseup: (e) => {
-    ['mousemove', 'mouseup', 'dragstart', 'dragend'].forEach(event => removeEvent(event, handlers[event]));
-    if (userSelecting) {
-      justSelected = true;
-      addEvent('click', handlers.click);
-      // console.log('add click event', userSelecting)
-    }
-    toggleUserSelect(e.target, false);
-  },
-  dragstart: (e) => {
-    if (userSelecting) return stopEvent(e);
-  },
-  dragend: (e) => ['dragend', 'mousemove', 'mouseup'].forEach(event => removeEvent(event, handlers[event])),
-  click: (e) => {
-    if (justSelected) {
-      justSelected = false;
-      setTimeout(() => {
-        removeEvent('click', handlers.click);
-        // console.log('remove click event', userSelecting)
-      }, 50);
-      return stopEvent(e);
-    }
-  },
-};
-
-const addEvent = (event, handler) => document.addEventListener(event, handler, true);
-const removeEvent = (event, handler) => document.removeEventListener(event, handler, true);
-
-const toggleUserSelect = (node, enable = false) => {
-  if (node) {
-    if (enable) node.classList.add('ext-Select-like-a-Boss');
-    else node.classList.remove('ext-Select-like-a-Boss');
-  }
-};
-
-document.addEventListener('mousedown', (e) => {
-  const excludeTags = e.target.closest('img, canvas, picture, svg, audio, video, object, progress, textarea')
-  let excludeTagsCount = 1;
-  if (excludeTags) excludeTagsCount = excludeTags.childElementCount;
-  if (e.button !== 0 || excludeTagsCount === 0) { excludeTagsCount = 1; return; }
-  userSelecting = false;
+const mainMouseDownHandler = (e) => {
+  let t = e.target
+  if (e.button !== 0 || getCurrentAnchor(t) === null) return // LMB on links only
+  // resetVars
+  needDetermineUserSelection = needCreateStartSelection = true;
+  userSelecting = needStopClick = false;
   cursor.x = e.clientX;
   cursor.y = e.clientY;
   if (selection.type === 'Range') {
     let range = getRangeFromPoint(cursor.x, cursor.y);
-    if (range && selection.getRangeAt(0).isPointInRange(range.startContainer, range.startOffset)) return;
+    if (range && selection.getRangeAt(0).isPointInRange(range.startContainer, range.startOffset)
+    ) return;
   }
-  let range = getRangeFromPoint(e.clientX, e.clientY);
-  if (range) {
-    selection.removeAllRanges();
-    selection.addRange(range);
-  }
-  toggleUserSelect(e.target, true);
-  ['mousemove', 'mouseup', 'dragstart', 'dragend'].forEach(event => addEvent(event, handlers[event]));
-}, true);
+  _letUserSelect();
+  if (t.nodeType === 3) t = t.parentNode
+  if (e.ctrlKey && regexTDTH.test(t.tagName) || e.altKey) return;
+  let n = getCurrentAnchor(t);
+  if (['HTMLTextAreaElement', 'HTMLCanvasElement'].includes(t.constructor.name) || t.textContent === '') return;
+  let rect = n.getBoundingClientRect();
+  movable = { n: n, x: Math.round(rect.left), y: Math.round(rect.top), c: 0 };
+  _bind(['mousemove', 'mouseup', 'dragend', 'dragstart']);
+  _letUserSelect(n);
+};
+
+// detection range setting
+let D = 3,
+  K = 0.8;
+function getOutFromMoveHandler() {
+  _unbind(['mousemove', 'mouseup', 'dragend', 'dragstart', 'click']);
+  _letUserSelect();
+  selection.removeAllRanges();
+}
+
+const handlers = {
+  mousemove: (e) => {
+    if (movable) {
+      if (movable.n.constructor !== HTMLAnchorElement && movable.n.draggable) {
+        movable = null;
+        return getOutFromMoveHandler();
+      }
+      if (movable.c++ < 12) {
+        let rect = movable.n.getBoundingClientRect();
+        if (
+          Math.round(rect.left) !== movable.x ||
+          Math.round(rect.top) !== movable.y
+        ) {
+          _unbind(['mousemove', 'mouseup', 'dragend', 'dragstart', 'click']);
+          _letUserSelect();
+          selection.removeAllRanges();
+          return;
+        }
+      } else movable = null;
+    }
+    let x = e.clientX;
+    let y = e.clientY;
+    if (needCreateStartSelection) {
+      if (!e.altKey || !e.ctrlKey) selection.removeAllRanges();
+      let correct = x > cursor.x ? -2 : 2;
+      let range = getRangeFromPoint(x + correct, y);
+      if (range) {
+        selection.addRange(range);
+        needCreateStartSelection = false;
+      }
+    }
+    if (needDetermineUserSelection) {
+      let vx = Math.abs(cursor.x - x),
+        vy = Math.abs(cursor.y - y);
+      userSelecting = vy === 0 || vx / vy > K;
+      if (vx > D || vy > D) {
+        needDetermineUserSelection = false;
+        if (userSelecting) {
+          needStopClick = true;
+          _bind('click');
+        }
+      }
+    }
+    if (userSelecting) {
+      let range = getRangeFromPoint(x, y);
+      if (range) selection.extend(range.startContainer, range.startOffset);
+    }
+  },
+  dragstart: (e) => {
+    _unbind('dragstart');
+    if (userSelecting) return stopEvent(e);
+  },
+  mouseup: (e) => {
+    _unbind(['mousemove', 'mouseup', 'dragstart', 'dragend']);
+    if (!userSelecting && needStopClick) needStopClick = false;
+    setTimeout(() => _unbind('click'), 111);
+    if (selection.type !== 'Range') _letUserSelect();
+  },
+  dragend: () => {
+    _unbind(['dragend', 'mousemove', 'mouseup']);
+  },
+  click: function (e) {
+    _unbind('click');
+    if (selection.type !== 'Range') _letUserSelect();
+    if (needStopClick) return stopEvent(e);
+  },
+};
+
+document.addEventListener('mousedown', mainMouseDownHandler, true);
